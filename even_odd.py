5771118dc61725e260abb70985826c575a5aa54d
ashik-bhuyan17 <ashik.ajke@gmail.com>
2022-05-14 23:22:10 +0600

    mm

--- even_odd.py
+++ even_odd.py
@@ -8,15 +8,15 @@ def is_even(num):
 starting = 0
 user_input = int(input("Limit : "))
 print(type(user_input))
-# while starting <= user_input:
-#     if is_even(starting):
-#         print(f"{starting} is even")
-#     else:
-#         print(f"{starting} is odd")
-#     starting+=1    
-
-for num in range(0,user_input):
-    if is_even(num):
-        print(f"{num} is even")
+while starting <= user_input:
+    if is_even(starting):
+        print(f"{starting} is even")
     else:
-        print(f"{num} is odd")
+        print(f"{starting} is odd")
+    starting+=1    
+
+# for num in range(0,user_input):
+#     if is_even(num):
+#         print(f"{num} is even")
+#     else:
+#         print(f"{num} is odd")
